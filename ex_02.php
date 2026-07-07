f4f2b1f55a1a024654df2abb4a49d5f1cf9167c0
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-07-07 12:03:22 -0300

    add - fiz o exercicio dois

---
+++ ex_02.php
@@ -0,0 +1,19 @@
+<!-- Exercício 02 – Espelho Mágico
+Uma empresa de tecnologia está desenvolvendo um sistema para tratamento de textos.
+Crie uma função chamada inverterTexto() que receba uma string e retorne o texto completamente invertido.
+Além disso, exiba a quantidade de caracteres existentes na string original.-->
+
+<?php
+function inverterTexto($texto){
+
+$textoInvertido = strrev($texto);
+$quantidadeCaracteres = strlen($texto);
+return array($textoInvertido, $quantidadeCaracteres);
+}
+
+$texto = "paralelamente";
+$resultado = inverterTexto($texto);
+
+echo "Texto original: $texto <br>";
+echo "Texto invertido: " . $resultado[0] . "<br>";
+echo "Quantidade de caracteres: " . $resultado[1];
